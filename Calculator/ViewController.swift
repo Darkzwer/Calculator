db3518fd34c28f42f20e14c0aa3e8284e1b12891
Darkzwer <52218993+Darkzwer@users.noreply.github.com>
2021-09-02 12:32:04 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Igor on 02/09/2021.
+//  Copyright © 2021 Simplesoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
